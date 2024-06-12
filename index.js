92560972ba81f4010c41e3bfed9d888e25da24ba
pad01g <101613202+pad01g@users.noreply.github.com>
2024-06-12 09:28:07 +0000

    add nodejs

---
+++ index.js
@@ -0,0 +1,26 @@
+const https = require("https");
+const express = require("express");
+const app = express();
+
+
+const PORT = process.env.PORT || 3000;
+const TOKEN = process.env.LINE_ACCESS_TOKEN;
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (req, res) => {
+    res.sendStatus(200);
+});
+
+app.post("/webhook", function (req, res) {
+    res.send("HTTP POST request sent to the webhook URL!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
